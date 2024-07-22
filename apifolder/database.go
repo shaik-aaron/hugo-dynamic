aedf2baf93dd33c0c5fe67a65755c4d056c294fc
shaik-aaron <shaikaaron.work@gmail.com>
2024-07-22 23:08:08 +0530

    refactor: added dependency injections

--- apifolder/database.go
+++ apifolder/database.go
@@ -2,12 +2,134 @@ package apifolder
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
 
 	_ "modernc.org/sqlite"
 )
 
 var db *sql.DB
+
+// Function Interfaces
+type AddNewLikeInterface interface {
+	AddNewLike(title string, user string, w http.ResponseWriter)
+}
+
+type AddNewCommentInterface interface {
+	AddNewComment(title string, user string, email string, comment string, w http.ResponseWriter)
+}
+
+type GetAllInterActionsInterface interface {
+	GetAllInteractions(title string) (int, []interactionData, error)
+}
+
+type interactionData struct {
+	User    string `json:"user"`
+	Comment string `json:"comment"`
+}
+
+// Structs
+type AddNewLikeFunction struct{}
+type AddNewCommentFunction struct{}
+type GetAllInteractionsFunction struct{}
+
+// Main Functions
+func (a AddNewLikeFunction) AddNewLike(title string, user string, w http.ResponseWriter) {
+	stmt, err := db.Prepare("INSERT INTO likes (title, user) VALUES (?, ?)")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(title, user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (ac AddNewCommentFunction) AddNewComment(title string, user string, email string, comment string, w http.ResponseWriter) {
+	stmt, err := db.Prepare("INSERT INTO comments (title, user, email, comment) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(title, user, email, comment)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (gal GetAllInteractionsFunction) GetAllInteractions(title string) (int, []interactionData, error) {
+	// Query for like count.
+	likeCountQuery := `
+		SELECT COUNT(*) 
+		FROM likes 
+		WHERE title = ?
+	`
+	var likeCount int
+	err := db.QueryRow(likeCountQuery, title).Scan(&likeCount)
+	if err != nil {
+		return 0, nil, fmt.Errorf("error querying like count: %w", err)
+	}
+
+	// Query for comments.
+	commentsQuery := `
+		SELECT user, comment 
+		FROM comments 
+		WHERE title = ?
+	`
+	rows, err := db.Query(commentsQuery, title)
+	if err != nil {
+		return 0, nil, fmt.Errorf("error querying comments: %w", err)
+	}
+	defer rows.Close()
+
+	var comments []interactionData
+	for rows.Next() {
+		var comment interactionData
+		if err := rows.Scan(&comment.User, &comment.Comment); err != nil {
+			return 0, nil, fmt.Errorf("error scanning comment: %w", err)
+		}
+		comments = append(comments, comment)
+	}
+
+	if err := rows.Err(); err != nil {
+		return 0, nil, fmt.Errorf("error iterating over comments: %w", err)
+	}
+
+	return likeCount, comments, nil
+}
+
+type AddNewLikeService struct {
+	AddNewLikeInterface AddNewLikeInterface
+}
+
+type AddNewCommentService struct {
+	AddNewCommentInterface AddNewCommentInterface
+}
+
+type GetAllInteractionsService struct {
+	GetAllInterActionsInterface GetAllInterActionsInterface
+}
+
+func (anl AddNewLikeService) AddNewLikeFinal(title string, user string, w http.ResponseWriter) {
+	anl.AddNewLikeInterface.AddNewLike(title, user, w)
+}
+
+func (anc AddNewCommentService) AddNewCommentFinal(title string, user string, email string, comment string, w http.ResponseWriter) {
+	anc.AddNewCommentInterface.AddNewComment(title, user, email, comment, w)
+}
+
+func (gal GetAllInteractionsService) GetAllInteractionsFinal(title string) (int, []interactionData, error) {
+	likeCount, comments, err := gal.GetAllInterActionsInterface.GetAllInteractions(title)
+	return likeCount, comments, err
+}
 
 func InitDB() {
 	var err error
--- apifolder/handlers.go
+++ apifolder/handlers.go
@@ -21,18 +21,9 @@ func AddLike(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	stmt, err := db.Prepare("INSERT INTO likes (title, user) VALUES (?, ?)")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(like.Title, like.User)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	sendLike := AddNewLikeFunction{}
+	sendLikeService := AddNewLikeService{AddNewLikeInterface: sendLike}
+	sendLikeService.AddNewLikeFinal(like.Title, like.User, w)
 
 	w.WriteHeader(http.StatusCreated)
 }
@@ -55,18 +46,9 @@ func AddComment(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	stmt, err := db.Prepare("INSERT INTO comments (title, user, email, comment) VALUES (?, ?, ?, ?)")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(comment.Title, comment.User, comment.Email, comment.Comment)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	sendComment := AddNewCommentFunction{}
+	sendCommentService := AddNewCommentService{AddNewCommentInterface: sendComment}
+	sendCommentService.AddNewCommentFinal(comment.Title, comment.User, comment.Email, comment.Comment, w)
 
 	w.WriteHeader(http.StatusCreated)
 }
@@ -80,59 +62,26 @@ func GetInteractions(w http.ResponseWriter, r *http.Request) {
 
 	title := r.URL.Query().Get("title")
 
-	// Get like count
-	likeCountQuery := `
-		SELECT COUNT(*) 
-		FROM likes 
-		WHERE title = ?
-	`
-	var likeCount int
-	err := db.QueryRow(likeCountQuery, title).Scan(&likeCount)
+	requestAllInteractions := GetAllInteractionsFunction{}
+	requestAllInteractionsService := GetAllInteractionsService{GetAllInterActionsInterface: requestAllInteractions}
+	likeCount, comments, err := requestAllInteractionsService.GetAllInteractionsFinal(title)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// Get comments
-	commentsQuery := `
-		SELECT user, comment 
-		FROM comments 
-		WHERE title = ?
-	`
-	rows, err := db.Query(commentsQuery, title)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	type interactionData struct {
-		User    string `json:"user"`
-		Comment string `json:"comment"`
-	}
-
+	// Prepare and send the response.
 	data := struct {
 		LikeCount int               `json:"like_count"`
 		Comments  []interactionData `json:"comments"`
 	}{
 		LikeCount: likeCount,
+		Comments:  comments,
 	}
 
-	for rows.Next() {
-		var comment interactionData
-		err := rows.Scan(&comment.User, &comment.Comment)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		data.Comments = append(data.Comments, comment)
-	}
-
-	err = rows.Err()
-	if err != nil {
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
-
-	json.NewEncoder(w).Encode(data)
 }

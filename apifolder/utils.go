1fd1d79ac99fdeae282745222253766fa8e70b90
shaik-aaron <shaikaaron.work@gmail.com>
2024-07-22 15:16:21 +0530

    initial commit

--- apifolder/utils.go
+++
@@ -1,9 +0,0 @@
-package apifolder
-
-import "net/http"
-
-func setCORSHeaders(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-}
--- identity/identitytesting/identitytesting.go
+++
@@ -1,5 +0,0 @@
-package identitytesting
-
-import "github.com/gohugoio/hugo/identity"
-
-const TestIdentity = identity.StringIdentity("__testIdentity")
